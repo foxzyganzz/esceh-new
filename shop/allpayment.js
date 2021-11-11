4d944a2710744a823d35a6e74634be638c9177e8
foxzyganzz <foxzyxganz212@gmail.com>
2021-11-11 20:19:52 +0700

    SC New Nih Broo SC Ori By Zeeone

---
+++ shop/allpayment.js
@@ -0,0 +1,29 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY FOXZY WAS HERE
+
+*さ BANK*
+1. BRI 
+	ㅁ -
+		• -
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0857 7916 7354
+		• FOX** S***E 2
+2. OVO
+	ㅁ 0813 8638 3559
+		• FOX** S***E
+3. DANA
+	ㅁ 0813 8638 3559
+		• FOX** S***E
+
+*さ QRIS ALL PAYMENT*
+
+PENCET LALU SS QRIS || MASUK KE APK E MONEY || LAKUKAN PEMBAYARAN :https://g.top4top.io/p_21400grog0.png
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
